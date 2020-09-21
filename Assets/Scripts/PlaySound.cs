fbffdffa84d89ea6b9dc7a0bef195cfc59d29b10
BrendanCranfield <61271276+BrendanCranfield@users.noreply.github.com>
2020-09-21 16:19:00 +0800

    School Completed (No NPC's)

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource audio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audio.PlayOneShot(audio.clip);
+    }
+
+    void PlaySoundClip()
+    {
+        audio.PlayOneShot(audio.clip);
+    }
+}
